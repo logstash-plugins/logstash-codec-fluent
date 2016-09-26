ccdb042e3953a374ae218fd7f30f8c928ecfae15
Daniel Malon <daniel.malon@me.com>
2016-09-26 11:53:16 +0100

    Upgrade message decoding to support newer Fluentd

--- spec/codecs/fluent_spec.rb
+++ spec/codecs/fluent_spec.rb
@@ -37,7 +37,60 @@ describe LogStash::Codecs::Fluent do
 
     it "should decode without errors" do
       subject.decode(message) do |event|
-       expect(event.get("name")).to eq("foo")
+        expect(event.get("name")).to eq("foo")
+      end
+    end
+
+  end
+
+  describe "event decoding (buckets of events)" do
+
+    let(:tag)       { "mytag" }
+    let(:epochtime) { event.timestamp.to_i }
+    let(:data)      { LogStash::Util.normalize(event.to_hash) }
+    let(:message) do
+      MessagePack.pack([tag,
+                        [
+                          [epochtime, data.merge(LogStash::Event::TIMESTAMP => event.timestamp.to_iso8601)],
+                          [epochtime, data.merge(LogStash::Event::TIMESTAMP => event.timestamp.to_iso8601)],
+                          [epochtime, data.merge(LogStash::Event::TIMESTAMP => event.timestamp.to_iso8601)]
+                        ]
+                       ])
+    end
+
+    it "should decode without errors" do
+      count = 0
+
+      subject.decode(message) do |event|
+        expect(event.get("name")).to eq("foo")
+        count += 1
+      end
+
+      expect(count).to eq(3)
+    end
+
+  end
+
+  describe "event decoding (broken package)" do
+
+    let(:tag)       { "mytag" }
+    let(:epochtime) { event.timestamp.to_s }
+    let(:data)      { LogStash::Util.normalize(event.to_hash) }
+    let(:message) do
+      MessagePack.pack([tag,
+                        epochtime, data.merge(LogStash::Event::TIMESTAMP => event.timestamp.to_iso8601)
+                       ])
+    end
+
+    it "should decode with errors" do
+      subject.decode(message) do |event|
+        expect(event.get("name")).not_to eq("foo")
+      end
+    end
+
+    it "should inject a failure event" do
+      subject.decode(message) do |event|
+        expect(event.get("tags")).to include("_fluentparsefailure")
       end
     end
 
