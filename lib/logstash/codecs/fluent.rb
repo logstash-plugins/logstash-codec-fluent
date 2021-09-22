1a2a6aebb93f04472d9c707a7d17e2af4f18e7d3
Ry Biesemeyer <yaauie@users.noreply.github.com>
2021-09-22 15:55:44 -0700

    decode: handle multiple PackedForward messages in a single payload (#28)
    MessagePack's `Unpacker` is stateful, and cannot be safely reused while
    iterating over results from a previous unpacking.
    
    Internally, `Unpacker#feed_each(input, &block)` buffers the provided input,
    and is documented as equivalent to sending `Unpacker#feed(input)` and then
    immediately sending `Unpacker#each(&block)`. When we are iterating over the
    items, we cannot safely reuse the in-progress unpacker because doing
    so feeds the packed payload to the _existing_ buffer.
    
    Instead, we must use our `Factory` to allocate a new `Unpacker`.
    
    There _may_ be optimizations to be had here around reusing an unpacker
    for PackedForward-internal unpacking, but these are outside the scope of this
    fix.

--- lib/logstash/codecs/fluent.rb
+++ lib/logstash/codecs/fluent.rb
@@ -117,7 +117,7 @@ class LogStash::Codecs::Fluent < LogStash::Codecs::Base
         raise(LogStash::Error, "PackedForward with compression is not supported")
       end
 
-      entries_decoder = @decoder
+      entries_decoder = @factory.unpacker
       entries_decoder.feed_each(entries) do |entry|
         yield generate_event(entry[1], entry[0], tag)
       end
